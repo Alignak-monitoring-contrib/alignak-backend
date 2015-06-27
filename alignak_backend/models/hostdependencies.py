4cc7eca321a1f30f8e9214d0dd97c89e9ca89fa5
David Durieux <d.durieux@siprossii.com>
2015-06-27 15:58:27 +0200

    Add hostdependencies

---
+++ alignak_backend/models/hostdependencies.py
@@ -0,0 +1,71 @@
+def get_name():
+    return 'hostdependencies'
+
+def get_schema():
+    return {
+        'schema': {
+            'imported_from': {
+                'type': 'string',
+                'default': 'unknown'
+            },
+
+            'use': {
+                'type': 'list',
+                'default': None
+            },
+
+            'name': {
+                'type': 'string',
+            },
+
+            'definition_order': {
+                'type': 'integer',
+                'default': 100
+            },
+
+            'register': {
+                'type': 'boolean',
+                'default': True
+            },
+
+            'dependent_host_name': {
+                'type': 'string'
+            },
+
+            'dependent_hostgroup_name': {
+                'type': 'string',
+                'default': ''
+            },
+
+            'host_name': {
+                'type': 'string'
+            },
+
+            'hostgroup_name': {
+                'type': 'string',
+                'default': 'unknown'
+            },
+
+            'inherits_parent': {
+                'type': 'boolean',
+                'default': False
+            },
+
+            'execution_failure_criteria': {
+                'type': 'list',
+                'default': ['n']
+            },
+
+            'notification_failure_criteria': {
+                'type': 'list',
+                'default': ['n']
+            },
+
+            'dependency_period': {
+                'type': 'string',
+                'default': ''
+            },
+        }
+    }
+
+
