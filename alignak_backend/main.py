72a5115f70d8c33ca61f00fe45b3ad85533a9853
David Durieux <d.durieux@siprossii.com>
2015-09-16 22:06:17 +0200

    Add docstring

--- alignak_backend/main.py
+++ alignak_backend/main.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Main
+"""
+
 from alignak_backend.app import Application
 
 
 def main():
+    """
+    Main function where run app
+
+    :return: None
+    """
     app = Application()
     app.process_args()
