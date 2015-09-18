a8f38b806435c101336497321ba575f7600c0a24
David Durieux <d.durieux@siprossii.com>
2015-09-18 16:18:53 +0200

    Add uipref endpoint

---
+++ alignak_backend/models/uipref.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Resource information of host
+"""
+
+
+def get_name():
+    """
+    Get name of this resource
+
+    :return: name of this resource
+    :rtype: str
+    """
+    return 'uipref'
+
+
+def get_schema():
+    """
+    Schema structure of this resource
+
+    :return: schema dictionnary
+    :rtype: dict
+    """
+    return {
+        'allow_unknown': True,
+        'schema': {
+            'contact': {
+                'type': 'objectid',
+                '_title': 'Contact',
+                'data_relation': {
+                    'resource': 'contact',
+                    'embeddable': True
+                },
+            },
+        }
+    }
