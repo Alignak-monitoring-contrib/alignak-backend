8772ed8df462e3d622fed5c1aef7c42ab55a407f
David Durieux <d.durieux@siprossii.com>
2015-09-18 16:34:42 +0200

    Update uipref schema

--- alignak_backend/models/uipref.py
+++ alignak_backend/models/uipref.py
@@ -32,6 +32,7 @@ def get_schema():
                     'resource': 'contact',
                     'embeddable': True
                 },
+                'required': True,
             },
         }
     }
