13b85f75b1f3dc315ec5354f80dd5fa813a20178
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-09-25 18:10:32 +0000

    Forgotten files in previous commit ...

---
+++ alignak_backend/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Module version"""
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
+import os
+del os.link
+
+import alignak_backend
+
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name="alignak_backend",
+    version=alignak_backend.__version__,
+
+    # metadata for upload to PyPI
+    author="Alignak team",
+    author_email="d.durieux@siprossii.com",
+    keywords="alignak monitoring",
+    url="https://github.com/Alignak-monitoring-contrib/alignak-backend",
+    description="Alignak REST Backend",
+    long_description=open('README.rst').read(),
+
+    packages = ['alignak_backend'],
+
+    install_requires=['Eve', 'flask-bootstrap'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration'
+    ]
 )
