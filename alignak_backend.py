182434f9d5dc0df78d4841780102f11612f068e6
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-10-20 10:15:10 +0200

    Update port number in comment

--- alignak_backend.py
+++ alignak_backend.py
@@ -23,6 +23,6 @@
     This file is used to run the Alignak backend in production environment with WSGI server.
 
     With uWSGI:
-        uwsgi --wsgi-file alignak_backend.py --callable app --socket 0.0.0.0:8090 --protocol=http --enable-threads
+        uwsgi --wsgi-file alignak_backend.py --callable app --socket 0.0.0.0:5000 --protocol=http --enable-threads
 """
 from alignak_backend.app import app
