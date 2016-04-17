403599141bfea68bebb5c87bf0ce509352a10503
David Durieux <d.durieux@siprossii.com>
2016-04-17 02:53:28 +0200

    Fix pep8 & pylint

--- alignak_backend/template.py
+++ alignak_backend/template.py
@@ -7,12 +7,11 @@
     This module manages the templates (host / services)
 """
 from __future__ import print_function
-from flask import current_app, g, request, abort, jsonify
+from flask import current_app, g, request, abort
 from eve.methods.post import post_internal
 from eve.methods.patch import patch_internal
-import json
-from bson.objectid import ObjectId
 from eve.render import send_response
+from bson.objectid import ObjectId
 from alignak_backend.models.host import get_schema as host_schema
 
 
@@ -22,19 +21,29 @@ class Template(object):
     """
 
     @staticmethod
-    def pre_post_host(request):
+    def pre_post_host(user_request):
         """
         If we use templates, we fill fields with template values
-        """
-        if isinstance(request.json, dict):
-            Template.fill_template_host(request.json)
-        else:
-            for i in request.json:
-                Template.fill_template_host(i)
 
+        :param user_request: request of the user
+        :type user_request: object
+        :return: None
+        """
+        if isinstance(user_request.json, dict):
+            Template.fill_template_host(user_request.json)
+        else:
+            for i in user_request.json:
+                Template.fill_template_host(i)
 
     @staticmethod
     def fill_template_host(item):
+        """
+        Prepare fields of host with fields of host templates
+
+        :param item: field name / values of the host
+        :type item: dict
+        :return: None
+        """
         host = current_app.data.driver.db['host']
         ignore_fields = ['_id', '_etag', '_updated', '_created', '_template_fields', '_templates',
                          '_is_template', 'realm']
@@ -42,24 +51,35 @@ class Template(object):
         for field_name, field_value in item.iteritems():
             fields_not_update.append(field_name)
         item['_template_fields'] = []
-        if '_is_template' not in item or not item['_is_template']:
-            if '_templates' in item and item['_templates'] != []:
-                for host_template in item['_templates']:
-                    hosts = host.find_one({'_id': ObjectId(host_template)})
-                    if hosts is not None:
-                        for field_name, field_value in hosts.iteritems():
-                            if field_name not in fields_not_update and field_name not in ignore_fields:
-                                item[field_name] = field_value
-                                item['_template_fields'].append(field_name)
-                schema = host_schema()
-                ignore_schema_fields = ['realm', '_template_fields', '_templates', '_is_template']
-                for key in schema['schema'].iterkeys():
-                    if key not in ignore_schema_fields:
-                        if key not in item:
-                            item['_template_fields'].append(key)
+        if ('_is_template' not in item or not item['_is_template']) \
+                and '_templates' in item and item['_templates'] != []:
+            for host_template in item['_templates']:
+                hosts = host.find_one({'_id': ObjectId(host_template)})
+                if hosts is not None:
+                    for field_name, field_value in hosts.iteritems():
+                        if field_name not in fields_not_update \
+                                and field_name not in ignore_fields:
+                            item[field_name] = field_value
+                            item['_template_fields'].append(field_name)
+            schema = host_schema()
+            ignore_schema_fields = ['realm', '_template_fields', '_templates', '_is_template']
+            for key in schema['schema'].iterkeys():
+                if key not in ignore_schema_fields:
+                    if key not in item:
+                        item['_template_fields'].append(key)
 
     @staticmethod
     def on_update_host(updates, original):
+        """
+        On update host, if not template, remove in '_template_fields' fields in updates because
+        we update these fields, so they are now not dependant of template
+
+        :param updates: modified fields
+        :type updates: dict
+        :param original: original fields
+        :type original: dict
+        :return: None
+        """
         if g.get('ignore_hook_patch', False):
             return
         if not original['_is_template']:
@@ -70,9 +90,18 @@ class Template(object):
                         original['_template_fields'].remove(field_name)
             updates['_template_fields'] = original['_template_fields']
 
-
     @staticmethod
     def on_updated_host(updates, original):
+        """
+        After host updated, if host is a template, report value of fields updated on host used
+        this template
+
+        :param updates: modified fields
+        :type updates: dict
+        :param original: original fields
+        :type original: dict
+        :return: None
+        """
         if g.get('ignore_hook_patch', False):
             g.ignore_hook_patch = False
             return
@@ -83,9 +112,17 @@ class Template(object):
             for host in hosts:
                 Template.update_host_use_template(host, updates)
 
-
     @staticmethod
     def update_host_use_template(host, fields):
+        """
+        This update (patch) host with values of template
+
+        :param host: fields / values of the host
+        :type host: dict
+        :param fields: fields updated in the template host
+        :type fields: dict
+        :return: None
+        """
         host_db = current_app.data.driver.db['host']
         template_fields = {}
         for template_id in host['_templates']:
