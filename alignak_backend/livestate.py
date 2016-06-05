d9df0996b573bd62256171376ff7e00f34b43c16
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-06-05 17:41:55 +0200

    Fixes #50

--- alignak_backend/livestate.py
+++ alignak_backend/livestate.py
@@ -57,9 +57,11 @@ class Livestate(object):
                     '_realm': item['realm'], 'name': item['name']}
             if item['initial_state'] == 'd':
                 data['state'] = 'DOWN'
+                data['state_id'] = 1
                 data['last_state'] = 'DOWN'
             elif item['initial_state'] == 'u':
                 data['state'] = 'UNREACHABLE'
+                data['state_id'] = 2
                 data['last_state'] = 'UNREACHABLE'
             post_internal("livestate", data)
 
@@ -99,12 +101,15 @@ class Livestate(object):
                     'name': host_info['name'] + '/' + item['name']}
             if item['initial_state'] == 'w':
                 data['state'] = 'WARNING'
+                data['state_id'] = 1
                 data['last_state'] = 'WARNING'
             elif item['initial_state'] == 'c':
                 data['state'] = 'CRITICAL'
+                data['state_id'] = 2
                 data['last_state'] = 'CRITICAL'
             elif item['initial_state'] == 'u':
                 data['state'] = 'UNKNOWN'
+                data['state_id'] = 3
                 data['last_state'] = 'UNKNOWN'
             post_internal("livestate", data)
 
