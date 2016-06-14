fa6130b6b23dcb330020baace5b3f7d37b06314e
David Durieux <d.durieux@siprossii.com>
2016-06-14 23:12:42 +0200

    Fix pylint

--- alignak_backend/app.py
+++ alignak_backend/app.py
@@ -376,6 +376,7 @@ def get_settings(prev_settings):
         os.path.abspath('./settings.json')
     ]
 
+    # pylint: disable=anomalous-backslash-in-string
     comment_re = re.compile(
         '(^)?[^\S\n]*/(?:\*(.*?)\*/[^\S\n]*|/[^\n]*)($)?',
         re.DOTALL | re.MULTILINE
