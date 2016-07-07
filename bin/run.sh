9e17fc842ec1a4c00785fde085c5eb7219ff7a4b
David Durieux <d.durieux@siprossii.com>
2016-07-07 21:58:18 +0200

    Update run.sh to run with 4 processes (better performances ^^ on multi-cores / processors)

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,1 @@
-uwsgi --plugin python --wsgi-file bin/alignak_backend.py --callable app --socket 0.0.0.0:5000 --protocol=http --enable-threads
+uwsgi --plugin python --wsgi-file bin/alignak_backend.py --callable app --socket 0.0.0.0:5000 --protocol=http --enable-threads -p 4
