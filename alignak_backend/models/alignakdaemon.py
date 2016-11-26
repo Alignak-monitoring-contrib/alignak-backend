f5984e2d78791796a1d0974c67264220b018f2e4
David Durieux <d.durieux@siprossii.com>
2016-11-26 21:12:37 +0100

    Endpoint to store Alignak daemon states. closes #173

---
+++ alignak_backend/models/alignakdaemon.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Resource information of Alignak daemons
+"""
+
+
+def get_name():
+    """
+    Get name of this resource
+
+    :return: name of this resource
+    :rtype: str
+    """
+    return 'alignakdaemon'
+
+
+def get_schema():
+    """
+    Schema structure of this resource
+
+    :return: schema dictionary
+    :rtype: dict
+    """
+    return {
+        'schema': {
+            'name': {
+                'type': 'string',
+                'required': True,
+                'empty': False,
+            },
+            'ip': {
+                'type': 'string',
+                'required': True,
+                'empty': False,
+            },
+            'port': {
+                'type': 'integer',
+                'required': True,
+                'empty': False,
+            },
+            'last_check': {
+                'type': 'datetime',
+                'required': True,
+                'empty': False,
+            },
+            'alive': {
+                'type': 'boolean',
+                'required': True,
+                'default': False
+            },
+            'reachable': {
+                'type': 'boolean',
+                'required': True,
+                'default': False
+            },
+            'passive': {
+                'type': 'boolean',
+                'required': True,
+                'default': False
+            },
+            'spare': {
+                'type': 'boolean',
+                'required': True,
+                'default': False
+            },
+            '_realm': {
+                'type': 'objectid',
+                'data_relation': {
+                    'resource': 'realm',
+                    'embeddable': True
+                },
+                'required': True,
+            },
+            '_users_read': {
+                'type': 'list',
+                'schema': {
+                    'type': 'objectid',
+                    'data_relation': {
+                        'resource': 'user',
+                        'embeddable': True,
+                    }
+                },
+            },
+            '_users_update': {
+                'type': 'list',
+                'schema': {
+                    'type': 'objectid',
+                    'data_relation': {
+                        'resource': 'user',
+                        'embeddable': True,
+                    }
+                },
+            },
+            '_users_delete': {
+                'type': 'list',
+                'schema': {
+                    'type': 'objectid',
+                    'data_relation': {
+                        'resource': 'user',
+                        'embeddable': True,
+                    }
+                },
+            },
+        }
+    }
