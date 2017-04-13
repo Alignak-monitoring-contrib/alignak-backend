a0906f38c1832bdf1838b229b890d1eb21d103b2
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-04-13 14:13:51 +0200

    Set default _sub_realm to True

--- alignak_backend/models/hostescalation.py
+++ alignak_backend/models/hostescalation.py
@@ -121,7 +121,7 @@ def get_schema():
             },
             '_sub_realm': {
                 'type': 'boolean',
-                'default': False
+                'default': True
             },
             '_users_read': {
                 'type': 'list',
--- alignak_backend/models/usergroup.py
+++ alignak_backend/models/usergroup.py
@@ -145,7 +145,7 @@ def get_schema():
                 "title": "Sub-realms",
                 "comment": "Is this element visible in the sub-realms of its realm?",
                 'type': 'boolean',
-                'default': False
+                'default': True
             },
 
             # Users CRUD permissions
