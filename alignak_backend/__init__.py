b5956896c1fa7f035ad69dcf3949ee02af4b4ba4
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-05-31 10:44:38 +0200

    Set version as 0.8.20

--- alignak_backend/__init__.py
+++ alignak_backend/__init__.py
@@ -8,7 +8,7 @@
     This module is an Alignak REST backend
 """
 # Application version and manifest
-VERSION = (0, 8, 19)
+VERSION = (0, 8, 20)
 
 __application__ = u"Alignak_Backend"
 __short_version__ = '.'.join((str(each) for each in VERSION[:2]))
